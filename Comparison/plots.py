aa6f5e1b2917766e714f96295d292e75be80d617
Vikram Singh <Vikram Singh>
2016-11-20 20:22:09 -0500

    Updated comparison folders

--- Comparison/plots.py
+++ Comparison/plots.py
@@ -2,11 +2,16 @@ from pylab import *
 import matplotlib.pyplot as plt
 import numpy as np
 
+font = {'family':'serif','weight':'bold','size':20}
+
+plt.rc('font', **font)
+plt.rc('text', usetex=True)
+
 diff = np.loadtxt('difference.txt',unpack=True)
 
-index = np.arange(0,15.1,0.1)
+index = np.arange(0,500,0.1)
 plt.bar(index,diff,0.1)
-plt.xlabel('Time [s]')
-plt.ylabel('% Difference')
-plt.title('Delta% C2(t) - U233 Pulse insertion')
+plt.xlabel(r'$ \rm Time \ [s]$')
+plt.ylabel(r'$ \rm \% \ Difference$')
+plt.title(r'$ \rm \Delta \% \ T_{f1}(t) \ - \ Simulink \ vs \ XCOS$')
 show()
